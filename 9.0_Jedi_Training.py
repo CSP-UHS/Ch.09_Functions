c7094fb0dfc83e36e762d63a28904653c4ff29bb
hermoniuscapensis <phamjul@urbandaleschools.com>
2020-01-09 22:55:03 -0600

    Just a commit

--- 9.0_Jedi_Training.py
+++ 9.0_Jedi_Training.py
@@ -1,23 +1,24 @@
-#Sign your name:________________
+# Sign your name:Julie
 
 
 #1.) Correct the following code: (The user's number should be increased by 1 and printed.)
 
 def increase(x):
-    return x + 1
- 
-num = input("Enter a number: ")
-increase(x)
-print("Your number has been increased to", x)
+    x = x + 1
+    return x
+
+num = int(input("Enter a number: "))
+thing = increase(num)
+print("Your number has been increased to", thing)
 
 
 
-#2.) Correct the following code to print 1-10:
+# #2.) Correct the following code to print 1-10:
 
-def count_to_ten:
-    for i in range[10]:
+def count_to_ten():
+    for i in range(1,10):
         print(i)
- 
+
 count_to_ten()
 
 
@@ -25,10 +26,13 @@ count_to_ten()
 #3.) Correct the following code to sum the list:
 
 def sum_list(list):
+    total = 0
     for i in list:
         sum = i
-        return sum
- 
+        total = total + sum
+    return total
+
+
 list = [45, 2, 10, -5, 100]
 print(sum_list(list))
 
@@ -40,30 +44,30 @@ print(sum_list(list))
 def reverse(text):
     result = ""
     text_length = len(text)
-    for i in range(text_length):
-        result = result + text[i * -1]
+    new_length = text_length + 1
+    for i in range(1, new_length):
+        result = result + text[i * -1] #from the right
     return result
- 
+
 text = input("Enter a sentence: ")
 print(reverse(text))
 
 
 
-#5.) Correct the following code: (if one of the options is not entered it should print the statements)
+# #5.) Correct the following code: (if one of the options is not entered it should print the statements)
 
 def get_user_choice():
-    while True:
-        command = input("Command: ")
-        if command = f or command = m or command = s or command = d or command = q:
-            return command
- 
+    command = input("Command: ")
+    if command == "f" or command == "m" or command == "s" or command == "d" or command == "q":
+        return command
+    else:
         print("Hey, that's not a command. Here are your options:" )
         print("f - Full speed ahead")
         print("m - Moderate speed")
         print("s - Status")
         print("d - Drink")
         print("q - Quit")
- 
+
 user_command = get_user_choice()
 print("You entered:", user_command)
 
