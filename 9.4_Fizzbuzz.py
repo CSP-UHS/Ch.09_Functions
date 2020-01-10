b56e588d84c26d0f5d002b0593dc46effc28e911
hermoniuscapensis <phamjul@urbandaleschools.com>
2020-01-10 00:01:26 -0600

    Just a commit

--- 9.4_Fizzbuzz.py
+++ 9.4_Fizzbuzz.py
@@ -34,3 +34,17 @@ fizzbuzz
 
 The classic test is to use the numbers 1-100 so make sure you test that with your function.
 '''
+def fizzbuzz(endpoint):
+    num = 0
+    for i in range(endpoint):
+        num+=1
+        if num % 3 == 0 and not num % 5 == 0:
+            print("fizz")
+        elif num % 5 == 0 and not num % 3 == 0:
+            print("buzz")
+        elif num % 3 == 0 and num % 5 == 0:
+            print("fizzbuzz")
+        else:
+            print(num)
+
+fizzbuzz(15)
