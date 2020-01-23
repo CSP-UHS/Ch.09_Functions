582bed3a2a655246a6c86803c21d11141e5c5fe8
School <floryken@urbandaleschools.com>
2020-01-23 13:48:10 -0600

    a committ

--- 9.0_Jedi_Training.py
+++ 9.0_Jedi_Training.py
@@ -1,52 +1,53 @@
-#Sign your name:________________
+# Sign your name:_Kenny Flory
 
-
-#1.) Correct the following code: (The user's number should be increased by 1 and printed.)
+'''
+# 1.) Correct the following code: (The user's number should be increased by 1 and printed.)
 
 def increase(x):
-    return x + 1
+    return x+1
  
-num = input("Enter a number: ")
-increase(x)
-print("Your number has been increased to", x)
-                        
+num = int(input("Enter a number: "))
+new=increase(num)
+print("Your number has been increased to", new)
+
  
 
 
 #2.) Correct the following code to print 1-10:
 
-def count_to_ten:
-    for i in range[10]:
+def count_to_ten():
+    for i in range(11):
         print(i)
  
 count_to_ten()
 
 
 
-#3.) Correct the following code to sum the list:
-
+# 3.) Correct the following code to sum the list:
 def sum_list(list):
-    for i in list:
-        sum = i
-        return sum
- 
-list = [45, 2, 10, -5, 100]
-print(sum_list(list))
+    sums = 0
+    for item in list:
+        sums += item
+    return sums
 
 
+old = [45, 2, 10, -5, 100]
+total=sum_list(old)
+print(total)
 
-
-#4.) Correct the following code which should reverse the sentence that is entered.
+# 4.) Correct the following code which should reverse the sentence that is entered.
 
 def reverse(text):
     result = ""
     text_length = len(text)
     for i in range(text_length):
-        result = result + text[i * -1]
+        result += text[(i+1) * -1]
     return result
- 
+
+
 text = input("Enter a sentence: ")
-print(reverse(text))
+new=reverse(text)
+print(new)
 
 
 
@@ -55,16 +56,19 @@ print(reverse(text))
 def get_user_choice():
     while True:
         command = input("Command: ")
-        if command = f or command = m or command = s or command = d or command = q:
+        if command.lower()=="f" or command.lower()=="m" or command.lower()=="s" or command.lower()=="d"\
+                or command.lower() == "q":
             return command
- 
-        print("Hey, that's not a command. Here are your options:" )
-        print("f - Full speed ahead")
-        print("m - Moderate speed")
-        print("s - Status")
-        print("d - Drink")
-        print("q - Quit")
+        else:
+            print()
+            print("Hey, that's not a command. Here are your options:" )
+            print("f - Full speed ahead")
+            print("m - Moderate speed")
+            print("s - Status")
+            print("d - Drink")
+            print("q - Quit")
  
 user_command = get_user_choice()
+print()
 print("You entered:", user_command)
-
+'''
---
+++ testing.py
@@ -0,0 +1,11 @@
+import math
+import random
+
+def print_name():
+    name="Kenny Flory"
+    return name
+
+print_name()
+
+if __name__ == "__main__":
+    main()
