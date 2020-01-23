4bfdd358f91343923bb4e6ac5c1d088755dc94d7
moritzemm <emmamor2323>
2020-01-23 13:41:42 -0600

    1/23/2020

--- trial.py
+++ trial.py
@@ -1,19 +1,27 @@
 import math
-
-'''def print_name():
+import random
+'''
+def print_name():
     """When called will print name""" #if you type help(print_name) it will print that
     name = "Emma E. Moritz"
-    return name'''
-
-
+    return name
 
 
 def volume_sphere(radius):
     volume=4/3*math.pi*radius**3
-    print(f"Your volume is: {volume:.2f} m^3")
+    return volume
 
 def myprogram():
-    volume_sphere(2)
+    v=volume_sphere(2)
+    print(v)
+____________________________________________________________________________
+'''
+def volume_cyl(radius,height,cans):
+    """This is a function to clculate the volume of a cylinder"""
+    volume=height*math.pi*radius**2*cans
+    print(volume)
+def main():
+    volume_cyl(1.31,4.75,20)
 
 if __name__ == "__main__":
-    myprogram()
+    main()
