13ea1ab4cb316e3124229c5394f12da794060619
halsteaddan <halsteaddan@urbandaleschools.com>
2020-01-24 14:19:14 -0600

    Fibonacci.

--- 9.5_Fibonacci.py
+++ 9.5_Fibonacci.py
@@ -15,9 +15,12 @@ def Fibonacci(endpoint):
     a = 0
     b = 1
     for i in range (0,endpoint,1):
-        print(a)
-        c = a + b
-        a = b
-        b = c
+        if a > endpoint:
+            break
+        else:
+            print(a)
+            c = a + b
+            a = b
+            b = c
 
 Fibonacci(100)
