3a86f39dc21b4f91550850231414cef32a24e5e6
Cal Watson <watsoncal@urbandaleschools.com>
2020-01-25 19:33:15 -0600

    Min Function Complete

--- 9.1_Min_Function.py
+++ 9.1_Min_Function.py
@@ -26,3 +26,27 @@ The function should return the value, not print the value.
 Also, while there is a min function built into Python, don't use it. 
 Please use if statements and practice creating it yourself.
 '''
+
+def min(a,b,c):
+    if a==b and a<c:
+        return a
+    elif a==c and a<b:
+        return a
+    elif b==c and b<a:
+        return b
+    elif a<b and a<c:
+        return a
+    elif b<a and b<c:
+        return b
+    elif c<a and c<a:
+        return c
+
+def main():
+    print(min(7, 3, 5))
+    print(min(5, 5, 4))
+    print(min(2, 2, 3))
+    print(min(-2, -6, -100))
+    print(min("Z", "B", "A"))
+
+if __name__ == "__main__":
+    main()
