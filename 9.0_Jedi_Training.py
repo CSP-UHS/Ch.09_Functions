cfcf06ea5531e351834cc452f52febc606261742
moritzemm <emmamor2323>
2020-02-03 14:08:57 -0600

    2/3/2020

--- 9.0_Jedi_Training.py
+++ 9.0_Jedi_Training.py
@@ -14,10 +14,10 @@ print("Your new number is: "y)
 
 #2.) Correct the following code to print 1-10:
 
-def count_to_ten(i):
+def count_to_ten():
     for i in range(1,11):
         print(i)
-count_to_ten(10)
+count_to_ten()
 
 
 
@@ -30,36 +30,51 @@ def sum_list(list):
     return sum
 list = [45, 2, 10, -5, 100]
 print(sum_list(list))
-'''
+
 #4.) Correct the following code which should reverse the sentence that is entered.
 
 def reverse(text):
     result = ""
     text_length = len(text)
     for i in range(text_length):
-        result = result + text[i * -1]
+        result = result + text[i * -1-1]
     return result
  
 text = input("Enter a sentence: ")
 print(reverse(text))
-
-
 '''
+
 #5.) Correct the following code: (if one of the options is not entered it should print the statements)
 
 def get_user_choice():
     while True:
         command = input("Command: ")
-        if command = f or command = m or command = s or command = d or command = q:
+        if command == "f" or command == "m" or command == "s" or command == "d" or command == "q":
             return command
- 
-        print("Hey, that's not a command. Here are your options:" )
-        print("f - Full speed ahead")
-        print("m - Moderate speed")
-        print("s - Status")
-        print("d - Drink")
-        print("q - Quit")
+        else:
+            print("Hey, that's not a command. Here are your options:" )
+            print("f - Full speed ahead")
+            print("m - Moderate speed")
+            print("s - Status")
+            print("d - Drink")
+            print("q - Quit")
  
 user_command = get_user_choice()
 print("You entered:", user_command)
-'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
