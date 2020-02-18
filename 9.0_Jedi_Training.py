c8fd1bbf01073d41c73f22122775a6343e41e9d0
catherineuhs <cliffordcat@urbandaleschools.com>
2020-02-18 10:03:11 -0600

    commit message

--- 9.0_Jedi_Training.py
+++ 9.0_Jedi_Training.py
@@ -1,70 +1,70 @@
-#Sign your name:Catherine
+#Sign your name:Catherine Clifford
 
 
 #1.) Correct the following code: (The user's number should be increased by 1 and printed.)
 
+
 def increase(x):
     return x + 1
- 
-num = input("Enter a number: ")
-increase(x)
-print("Your number has been increased to", x)
-                        
- 
 
+num = int(input("Enter a number: "))
+num2 = increase(num)
+print("Your number has been increased to", num2)
 
 #2.) Correct the following code to print 1-10:
 
-def count_to_ten:
-    for i in range[10]:
+
+def count_to_ten():
+    for i in range(1, 11):
         print(i)
- 
+
 count_to_ten()
-
-
 
 #3.) Correct the following code to sum the list:
 
+
 def sum_list(list):
-    for i in list:
-        sum = i
-        return sum
- 
+    sum = 0
+    for item in list:
+        sum += item
+    return sum
+
 list = [45, 2, 10, -5, 100]
 print(sum_list(list))
 
-
-
-
 #4.) Correct the following code which should reverse the sentence that is entered.
+
 
 def reverse(text):
     result = ""
     text_length = len(text)
     for i in range(text_length):
-        result = result + text[i * -1]
+        result = result + text[i -1]
     return result
- 
-text = input("Enter a sentence: ")
+
+text = str(input("Enter a sentence: "))
 print(reverse(text))
 
 
 
-#5.) Correct the following code: (if one of the options is not entered it should print the statements)
+# 5.) Correct the following code: (if one of the options is not entered it should print the statements)
 
 def get_user_choice():
     while True:
+        print()
         command = input("Command: ")
-        if command = f or command = m or command = s or command = d or command = q:
+        if command.lower() == "f" or command.lower() == "m" or command.lower() == "s" or command.lower() == "d" or command.lower() == "q":
             return command
- 
-        print("Hey, that's not a command. Here are your options:" )
-        print("f - Full speed ahead")
-        print("m - Moderate speed")
-        print("s - Status")
-        print("d - Drink")
-        print("q - Quit")
- 
-user_command = get_user_choice()
-print("You entered:", user_command)
+        else:
+            print()
+            print("Hey, that's not a command. Here are your options:")
+            print("f - Full speed ahead")
+            print("m - Moderate speed")
+            print("s - Status")
+            print("d - Drink")
+            print("q - Quit")
 
+
+user_command = get_user_choice()
+
+print("You entered:", user_command)
