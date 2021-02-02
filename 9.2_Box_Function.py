b497a28dab9f120021affcdc42f9e57a6dd5af1e
schmidtjos <schmidtjos@urbandaleschools.com>
2021-02-02 10:18:32 -0600

    finished 9.2

--- 9.2_Box_Function.py
+++ 9.2_Box_Function.py
@@ -31,3 +31,56 @@ oooooooooo
 oooooooooo
 oooooooooo
 '''
+
+
+def program():
+    global running
+    running = True
+
+    def cbox():
+        global running
+        while running:
+            base = int(input("Base: "))
+            height = int(input("Height: "))
+            print()
+            for i in range(height):
+                print("o" * base + "\r")
+            print()
+            print("Run again?")
+            print("[Y] [N]")
+            print()
+            choice = input("Input: ")
+            print()
+            if choice.lower() == "n":
+                running = False
+
+    def box(height, base):
+        for i in range(height):
+            print("o" * base + "\r")
+
+    while True:
+        print("Would you like to run a custom box, or a preset grouping of boxes?")
+        print("[C] [P]")
+        print()
+        choice = input("Input: ")
+        print()
+        if choice.lower() == "c":
+            cbox()
+        elif choice.lower() == "p":
+            box(7, 5)
+            print()
+            box(3, 2)
+            print()
+            box(3, 10)
+            print()
+            print("Run again?")
+            print("[Y] [N]")
+            print()
+            choice = input("Input: ")
+            print()
+            if choice.lower() == "n":
+                running = False
+
+
+if __name__ == "__main__":
+    program()
