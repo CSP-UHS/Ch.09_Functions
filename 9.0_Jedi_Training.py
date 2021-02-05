9a96b67854b8d8dcdccc562bb81689995df8054c
muetzelrya <muetzelrya@urbandaleschools.com>
2021-02-05 10:38:41 -0600

    9.0 jedi training complete

--- 9.0_Jedi_Training.py
+++ 9.0_Jedi_Training.py
@@ -1,61 +1,60 @@
-#Sign your name:________________
+# Sign your name: Ryan Muetzel
 
 
-#1.) Correct the following code: (The user's number should be increased by 1 and printed.)
+# 1.) Correct the following code: (The user's number should be increased by 1 and printed.)
 
 def increase(x):
     return x + 1
- 
-num = input("Enter a number: ")
-increase(x)
-print("Your number has been increased to", x)
-                        
- 
 
 
-#2.) Correct the following code to print 1-10:
+num = int(input("Enter a number: "))
+increase(num)
+print("Your number has been increased to", increase(num))
 
-def count_to_ten:
-    for i in range[10]:
-        print(i)
- 
+
+# 2.) Correct the following code to print 1-10:
+
+def count_to_ten():
+    for i in range(10):
+        print(i+1)
+
+
 count_to_ten()
 
 
-
-#3.) Correct the following code to sum the list:
+# 3.) Correct the following code to sum the list:
 
 def sum_list(list):
-    for i in list:
-        sum = i
-        return sum
- 
+    x = 0
+    for i in range(len(list)):
+        x += list[i]
+    return x
+
+
 list = [45, 2, 10, -5, 100]
 print(sum_list(list))
 
 
-
-
-#4.) Correct the following code which should reverse the sentence that is entered.
+# 4.) Correct the following code which should reverse the sentence that is entered.
 
 def reverse(text):
     result = ""
-    text_length = len(text)
-    for i in range(text_length):
-        result = result + text[i * -1]
+    for i in range(len(text)):
+        result += text[len(text) - 1 - i]
     return result
- 
+
+
 text = input("Enter a sentence: ")
 print(reverse(text))
 
 
-
-#5.) Correct the following code: (if one of the options is not entered it should print the statements)
+# 5.) Correct the following code: (if one of the options is not entered it should print the statements)
 
 def get_user_choice():
     while True:
         command = input("Command: ")
-        if command = f or command = m or command = s or command = d or command = q:
+        if command.upper()[:1] == 'F' or command.upper()[:1] == 'M' or command.upper()[:1] == 'S' \
+                or command.upper()[:1] == 'D' or command.upper()[:1] == 'Q':
             return command
  
         print("Hey, that's not a command. Here are your options:" )
@@ -64,7 +63,8 @@ def get_user_choice():
         print("s - Status")
         print("d - Drink")
         print("q - Quit")
- 
+
+
 user_command = get_user_choice()
 print("You entered:", user_command)
 
