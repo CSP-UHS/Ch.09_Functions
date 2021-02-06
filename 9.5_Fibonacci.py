73b755c76e81c1cb7dfd9206b73b238c01a907db
muetzelrya <muetzelrya@urbandaleschools.com>
2021-02-06 09:44:48 -0600

    9.5 fibonacci complete

--- 9.5_Fibonacci.py
+++ 9.5_Fibonacci.py
@@ -10,3 +10,15 @@ in the Fibonacci sequence. Pass the number into the function.
 Just to do a quick review of text formatting in the last chapter, make the list of numbers
 right-justified with commas.
 '''
+
+
+def fibonacci(max):
+    num = 1
+    last_num = 0
+    for i in range(max):
+        print(num)
+        num += last_num
+        last_num = num - last_num
+
+
+fibonacci(50)
