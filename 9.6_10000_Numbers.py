3909f043714f2ed93bed65a1e6598d83c880dcbb
Geni W <williamsgen@urbandaleschools.com>
2021-02-18 10:17:47 -0600

    Ch 9 done

--- 9.6_10000_Numbers.py
+++ 9.6_10000_Numbers.py
@@ -47,8 +47,12 @@ OUTPUT
 ------
 3 
 '''
-import random
-
+def count_list(my_list,n):
+    count=0
+    for item in my_list:
+        if item == n:
+            count += 1
+    return count
 
 my_list = [1,2,3,3,3,4,2,1]
 count = count_list(my_list,3)
@@ -70,11 +74,25 @@ OUTPUT
 ------
 2.0
 '''
+def average_list(my_list):
+    total = 0
+    for item in my_list:
+        total += item
+    avg = total/len(my_list)
+    return f"{avg:.1f}"
 
-
+my_list = [1,2,3]
+avg = average_list(my_list)
+print(avg)
 '''
 Now that the functions have been created, use them all in a main program that will:
 1.) Create a list of 10,000 random numbers from 1 to 6. (1 line of code)
 2.) Print the count of 1 through 6. (For example, "There are 1361 amount of 2s") (3 lines of code)
 3.) Print the average of all 10,000 random numbers. (Make sure it's a float) (2 lines of code)
 '''
+my_list = create_list(10000)
+for i in range(1,7):
+    count = count_list(my_list,i)
+    print("There are", count, "amount of", i)
+avg = average_list(my_list)
+print(avg)
