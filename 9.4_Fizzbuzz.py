bb76fb6f7f5fb2c069861a945aec8873e6aebf3c
getzbry <getzbry@urbandaleschools.com>
2021-02-18 10:24:29 -0600

    done

--- 9.4_Fizzbuzz.py
+++ 9.4_Fizzbuzz.py
@@ -34,3 +34,22 @@ fizzbuzz
 
 The classic test is to use the numbers 1-100 so make sure you test that with your function.
 '''
+import random
+def fizzbuzz(endpoint):
+    for num in range(1, endpoint+1):
+        if num%15 == 0:
+            print("fizzbuzz")
+        elif num%5 == 0:
+            print("buzz")
+        elif num%3 == 0:
+            print("fizz")
+        else:
+            print(num)
+
+print(random)
+
+def main():
+    fizzbuzz(100)
+
+if __name__=="__main__":
+    main()
