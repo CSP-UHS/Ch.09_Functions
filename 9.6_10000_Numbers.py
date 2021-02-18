e0b84e2eb72c8089cf14eed03acaa934a15e080f
dautom21 <dautom@urbandaleschools.com>
2021-02-18 09:58:45 -0600

    9.7 BB8 Finished

--- 9.6_10000_Numbers.py
+++ 9.6_10000_Numbers.py
@@ -19,7 +19,17 @@ OUTPUT
 ------
 [2,5,1,6,3] #something like this 
 '''
+import random
 
+
+def create_list(num):
+    my_list = []
+    for i in range(num):
+        my_list.append(random.randint(1, 6))
+    return my_list
+
+
+print(create_list(5))
 
 
 
