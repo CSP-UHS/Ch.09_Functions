c86459c2e9c372f97b4c74acf245eeee33ee6a06
maassjax <maassjax@urbandaleschools.com>
2022-01-12 13:16:37 -0600

    Function

--- 9.0_Jedi_Training.py
+++ 9.0_Jedi_Training.py
@@ -1,4 +1,4 @@
-#Sign your name:________________
+#Sign your name:Jaxson Maass
 
 
 #1.) Correct the following code: (The user's number should be increased by 1 and printed.)
@@ -6,29 +6,29 @@
 def increase(x):
     return x + 1
  
-num = input("Enter a number: ")
-increase(x)
-print("Your number has been increased to", x)
+num = int(input("Enter a number: "))
+newnum = increase(num)
+print("Your number has been increased to", newnum)
                         
  
 
 
 #2.) Correct the following code to print 1-10:
 
-def count_to_ten:
-    for i in range[10]:
+def count_to_ten(x):
+    for i in range(1,x+1):
         print(i)
- 
-count_to_ten()
+count_to_ten(100)
 
 
 
 #3.) Correct the following code to sum the list:
 
 def sum_list(list):
+    sum=0
     for i in list:
-        sum = i
-        return sum
+        sum += i
+    return sum
  
 list = [45, 2, 10, -5, 100]
 print(sum_list(list))
@@ -41,7 +41,7 @@ print(sum_list(list))
 def reverse(text):
     result = ""
     text_length = len(text)
-    for i in range(text_length):
+    for i in range(1,text_length+1):
         result = result + text[i * -1]
     return result
  
@@ -55,7 +55,7 @@ print(reverse(text))
 def get_user_choice():
     while True:
         command = input("Command: ")
-        if command = f or command = m or command = s or command = d or command = q:
+        if command == "f" or command == "m" or command == "s" or command == "d" or command == "q":
             return command
  
         print("Hey, that's not a command. Here are your options:" )
