b89c7cae5232784aaa9e85f44f5e6a49477ec820
Joseph Jarman <57240500+Jarmander333@users.noreply.github.com>
2022-01-12 14:03:45 -0600

    Update 9.2_Box_Function.py

--- 9.2_Box_Function.py
+++ 9.2_Box_Function.py
@@ -31,3 +31,17 @@ oooooooooo
 oooooooooo
 oooooooooo
 '''
+
+def box(y, x):
+	for i in range(y):
+		print("o"*x)
+
+def main():
+	box(7,5) # Print a box 7 high, 5 across
+	print() # Blank line
+	box(3,2) # Print a box 3 high, 2 across
+	print() # Blank line
+	box(3,10) # Print a box 3 high, 10 across
+
+if __name__ == "__main__":
+	main()
