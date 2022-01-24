d40649878534b3b554c27fe4cc7a1ee93c743237
Flare Flare Flare <flyrmat@urbandaleschools.com>
2022-01-24 13:04:32 -0600

    done

--- 9.7_BB8.py
+++ 9.7_BB8.py
@@ -1,26 +1,13 @@
-'''
-BB8 DRAWING PROGRAM
--------------------
-Back to the drawing board! Get it? Let's say we want to draw many BB8 robots
-of varying sizes at various locations. We can make a function called draw_BB8().
-We've made some basic changes to our original drawing program. We still have the
-first two lines as importing arcade and opening an arcade window. We actually took 
-all of the other drawing code and put it in a function called main(). At the bottom
-we call the main() function. In the main() function we call the draw_BB8() function
-multiple times. We pass three parameters to it: x, y and radius. Write the code for 
-the draw_BB8() function so that the resulting picture looks as close as you can get
-it to the one on the website.
-'''
-
-# Imports arcade module
+z
 import arcade
 
 # Opens a 600px by 600px window and puts BB8 in the title
 arcade.open_window(600, 600, "BB8")
-
+#light steel blue, orange, white, blue gray
 # Function to draw BB8 robots
 def draw_BB8(x,y, radius):
-  
+    arcade.draw_circle_filled(x,y,radius,arcade.color.WHITE)
+    arcade.draw_circle_outline(x,y,radius,arcade.color.BLACK)
 
 '''The main function where we set background color, start and finish rendering and run.'''
 def main():
